ebea6e021907d90fd9fa1268de020248a9d3005c
Naveen Kanikaram <nkanikaram@gmail.com>
2025-06-01 03:13:57 -0500

    Resetting the changes completely.

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -5,9 +5,8 @@ namespace stocks.api
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
-        public int TemperatureD { get; set; }
 
-        public int TemperatureF => 32111 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
