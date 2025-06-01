7201ca50c907b22e31236306e5170ab58f64c53a
Naveen Kanikaram <nkanikaram@gmail.com>
2025-06-01 03:08:28 -0500

    Committing Stashed Master

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -5,6 +5,7 @@ namespace stocks.api
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
+        public int TemperatureD { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
