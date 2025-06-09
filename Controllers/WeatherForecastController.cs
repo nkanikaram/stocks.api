a2e73aa5e44a5c78c30fd4191af1a3cd464eb5e6
Naveen Kanikaram <nkanikaram@gmail.com>
2025-06-09 15:47:44 -0500

    Test comment commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,6 +23,7 @@ namespace stocks.api.Controllers
         [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
+            //Adding comment
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
